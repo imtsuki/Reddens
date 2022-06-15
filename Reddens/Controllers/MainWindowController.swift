33b49bd51717610f78123c25b6a6ce81b0082aaf
imtsuki <me@qjx.app>
2022-06-15 01:04:31 -0400

    sweet fullscreen animation 🤤
    Signed-off-by: imtsuki <me@qjx.app>

--- Reddens/Controllers/MainWindowController.swift
+++ Reddens/Controllers/MainWindowController.swift
@@ -1,6 +1,40 @@
 import Cocoa
 
-class MainWindowController: NSWindowController {
+class MainWindowController: NSWindowController, NSWindowDelegate {
+    var priorWindowFrame: NSRect?
+
+    func customWindowsToEnterFullScreen(for window: NSWindow) -> [NSWindow]? {
+        return [window]
+    }
+
+    func customWindowsToExitFullScreen(for window: NSWindow) -> [NSWindow]? {
+        return [window]
+    }
+
+    func window(_ window: NSWindow, startCustomAnimationToEnterFullScreenOn screen: NSScreen, withDuration duration: TimeInterval) {
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = duration
+            window.animator().setFrame(screen.frame, display: true, animate: true)
+        }, completionHandler: nil)
+    }
+
+    func window(_ window: NSWindow, startCustomAnimationToExitFullScreenWithDuration duration: TimeInterval) {
+        if NSMenu.menuBarVisible() {
+            NSMenu.setMenuBarVisible(false)
+        }
+
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = duration
+            window.animator().setFrame(priorWindowFrame!, display: true, animate: true)
+        }, completionHandler: nil)
+
+        NSMenu.setMenuBarVisible(true)
+    }
+
+    func windowWillEnterFullScreen(_ notification: Notification) {
+        priorWindowFrame = self.window?.frame
+    }
+
     @IBAction func openFile(_ sender: Any) {
         let panel = NSOpenPanel()
         panel.title = "Choose 3D Model"
